f8a66cf5697eb1a0389d5ec11768ca489a7a27a3
MichiDevelop <michidevelop@gmail.com>
2023-01-28 23:05:20 -0600

    Seccion 8: (Server) 78.Middleware personalizado - ValidarCampos

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,15 +4,6 @@ const { validationResult } = require("express-validator")
 
 const crearUsuario = (req, res = response) => {
 
-    const errores = validationResult(req);
-
-    if(!errores.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        })
-    }
-
     res.json({
         ok: true,
         msg: 'Crear Usuario!!!'
--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,16 @@ const {Router} = require('express');
 const { check } = require('express-validator');
 
 const { crearUsuario } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 
 router.post('/new',[
     check('usuario','El usuario es obligatorio').not().isEmpty(),
+    check('email','El correo es obligatorio').isEmail(),
+    check('password','El password es obligatorio').isLength({min:5}),
+    validarCampos,
 ] ,crearUsuario)
 
 
